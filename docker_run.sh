b1bd9aa34a06fb2307387f7a6e84d341dd34cab8
Michael Pilosov <consistentbayes@gmail.com>
2019-09-24 17:19:43 -0600

    default ports and name

--- docker_run.sh
+++ docker_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # choose name for your container and image from which it will be built
-CONTAINER_NAME=math-user
+CONTAINER_NAME=math-user-local
 IMAGE_NAME=math-user:local
 # choose an available port
-PORT=8888
+PORT=8889
 # mount files associated with personal settings 
 MOUNT_PATH=$HOME
 # working directory will be symlinked to /work inside of container
