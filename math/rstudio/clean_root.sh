0158eb08886daaaa236ed9d6f0bfcdfa108204ef
mathematicalmichael <mpilosov@gmail.com>
2019-11-02 15:15:53 -0600

    cleanup on R.

--- math/rstudio/clean_root.sh
+++
@@ -1,6 +0,0 @@
-# clean up extra files from juptyerlab and apt
-rm -rf $CONDA_DIR/share/jupyter/lab/staging && \
-    rm -rf /home/$NB_USER/.cache/yarn && \
-    apt-get -qq purge && \
-    apt-get -qq clean && \
-    rm -rf /var/lib/apt/lists/*
