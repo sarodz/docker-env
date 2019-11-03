88c09b80a2c68033398b1ce4adc6b38c7169d7c0
mathematicalmichael <mpilosov@gmail.com>
2019-11-03 13:08:52 -0700

    last files.

---
+++ math/python/tex/tex_root.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Latex-related additions
+# the following do not appear in docker-stacks/minimal-notebook:
+apt-get update && \
+    apt-get install -y --no-install-recommends \
+    context \
+    dvipng \
+    fonts-lato \
+    fonts-dejavu \
+    ghostscript \
+    # texlive \
+    texlive-latex-recommended \
+    texlive-luatex \
+    texlive-science \
+    latexmk \
+    ;
+# build font caches
+mtxrun --script fonts --reload luaotfload-tool --update
