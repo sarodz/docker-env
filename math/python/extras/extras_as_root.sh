afaf5b5d91424abdbd1cbd1dcfe9b98020359b19
mathematicalmichael <mpilosov@gmail.com>
2019-11-03 15:58:06 -0700

    more cleaning stages to be safe/small

--- math/python/extras/extras_as_root.sh
+++ math/python/extras/extras_as_root.sh
@@ -24,6 +24,7 @@ apt-get update && apt-get install -yq --no-install-recommends \
     unzip \
     nano
 
+
 if $INCL_EXTRAS; then 
 # The following are things that I personally like having available
     apt-get update && \
@@ -37,12 +38,7 @@ if $INCL_EXTRAS; then
         less \
         curl \
         ;
-    
-    rm -rf /var/lib/apt/lists/*
-    # note that the curl above is for the user, the one below is run by root
-    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
-    dpkg -i ripgrep*.deb
-    rm -rf ripgrep*.deb
+    rm -rf /var/lib/apt/lists/* && apt-get clean
 fi
 
 # TODO:
--- math/python/py_root.sh
+++ math/python/py_root.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-
+# the fastest search
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+dpkg -i ripgrep*.deb
+rm -rf ripgrep*.deb
+# fuzzy finder
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
