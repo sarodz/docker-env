c8e4b05db82925ba695b98581a7aacfaf388f607
Michael Pilosov <consistentbayes@gmail.com>
2019-11-05 16:46:37 +0000

    updates

--- sagemaker/local_test/predict.sh
+++ sagemaker/local_test/predict.sh
@@ -3,5 +3,5 @@
 payload=$1
 content=${2:-text/csv}
 
-curl --data-binary @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
-#curl --data-binary @${payload} -H "Content-Type: ${content}" -v https://runtime.sagemaker.us-west-2.amazonaws.com/endpoints/decision-trees-example-small/invocations
+#curl --data-binary @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
+curl --data-binary @${payload} -H "Content-Type: ${content}" -v https://biz.mathfight.club/invocations
