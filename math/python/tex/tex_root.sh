282e04678225e6a31e2ab85a81a69bfb0b4f012d
mathematicalmichael <mpilosov@gmail.com>
2019-12-27 11:30:20 -0700

    latex build

--- math/python/tex/tex_root.sh
+++ math/python/tex/tex_root.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 # Latex-related additions
-# the following do not appear in docker-stacks/minimal-notebook:
 apt-get update && \
     apt-get install -y --no-install-recommends \
+    apt-transport-https \
+    ca-certificates \
+    build-essential \
+    locales \
+    tzdata \
+    lmodern \
+    unzip \
+    ;
+
+apt-get install -y --no-install-recommends \
     context \
     dvipng \
     fonts-lato \
     fonts-dejavu \
     ghostscript \
-    # texlive \
+    texlive-fonts-recommended \
+    texlive-fonts-extra \
+    texlive-generic-recommended \
+    texlive-latex-base \
     texlive-latex-recommended \
-    texlive-luatex \
+    texlive-latex-extra \
     texlive-science \
     latexmk \
+    ghostscript \
+    fonts-lato \
+    fonts-dejavu \
+    texlive-xetex \
+    texlive-luatex \
     ;
+
 # build font caches
 mtxrun --script fonts --reload luaotfload-tool --update
