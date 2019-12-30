fd9157f8076a12498a79fa1128413904b4603f9e
Michael Pilosov <consistentbayes@gmail.com>
2019-12-29 19:38:53 -0700

    fzf

---
+++ mac-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install htop tmux reattach-to-user-namespace
+brew install pass pinentry-mac
+brew install diff-so-fancy ripgrep
+brew install fzf
+
+
