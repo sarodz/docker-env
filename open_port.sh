b38a907cb6863b0da3957834ccd46db803e2d77d
Michael Pilosov <consistentbayes@gmail.com>
2020-01-19 14:39:23 -0700

    jrnl

--- open_port.sh
+++ open_port.sh
@@ -1,5 +1,7 @@
-MAPPING_NAME=nameformapping
-PORT_IN_VM=8888
-PORT_FWD=80
+MAPPING_NAME=firefox
+PORT_IN_VM=5800
+PORT_FWD=5800
 VBOX_NAME=default
-vboxmanage controlvm $VBOX_NAME --natpf1 "$MAPPING_NAME,tcp,,$PORT_IN_VM,,$PORT_FWD"
+vboxmanage controlvm $VBOX_NAME natpf1 "${MAPPING_NAME},tcp,,${PORT_IN_VM},,${PORT_FWD}"
+
+# source: https://stackoverflow.com/questions/35372399/connect-to-docker-machine-using-localhost
